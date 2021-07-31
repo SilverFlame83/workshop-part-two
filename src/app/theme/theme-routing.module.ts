ba541db432a367f868d3f40ac1ef09754880f6c5
SilverFlame83 <imren_83@abv.bg>
2021-07-31 13:27:19 +0300

    Last changes

--- src/app/theme/theme-routing.module.ts
+++ src/app/theme/theme-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
                 component:ThemesComponent
             },
             {
-                path: 'themeId',
+                path: ':themeId',
                 component:ThemeComponent
             },
             {
--- src/app/theme/theme/theme.component.ts
+++ src/app/theme/theme/theme.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-// import { ContentService } from '../../content.service';
-// import { ITheme } from '../../shared/interfaces';
+import { ActivatedRoute } from '@angular/router';
+import { ContentService } from '../../content.service';
+import { ITheme } from '../../shared/interfaces';
 
 @Component({
   selector: 'app-theme',
@@ -9,14 +10,18 @@ import { Component } from '@angular/core';
 })
 export class ThemeComponent {
 
-  // themes: ITheme[] | undefined;
+  theme: ITheme | undefined;
 
-  // constructor(private contentService: ContentService) {
-  //   this.fetchThemes();
-  // }
+  constructor(
+    private contentService: ContentService,
+    private activatedRoute: ActivatedRoute
+    ) {
+    this.fetchTheme();
+  }
 
-  // fetchThemes(): void {
-  //   this.themes = undefined;
-  //   this.contentService.loadThemes().subscribe(themes => this.themes = themes);
-  // }
+  fetchTheme(): void {
+    this.theme = undefined;
+    const id = this.activatedRoute.snapshot.params.themeId;
+    this.contentService.loadTheme(id).subscribe(theme => this.theme = theme);
+  }
 }
